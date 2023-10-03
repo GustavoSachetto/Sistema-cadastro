2d918fd77cd84ee7e2aecf1deb0cb18f1fb831c6
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-03 20:25:43 -0300

    Update DML - Inserção.sql

--- DOCS/DML - Inserção.sql
+++ DOCS/DML - Inserção.sql
@@ -1,12 +1,22 @@
 insert into categoria (tipo, id) values
-("Ação", 1),
-("Esportes", 2),
-("Filmes e séries", 3);
+("Esporte", 1),
+("Ação", 2),
+("Filmes e séries", 3),
+("Aventura", 4),
+("Drama", 5),
+("Comédia", 6),
+("Terror", 7);
 
 insert into noticia (titulo, conteudo, codNoticia, idCategoria) values
-("Bombeiros retornam", "O Corpo de Bombeiros retoma neste domingo (1º) as buscas por um homem que desapareceu após ser levado por enxurrada na Zona Leste de São Paulo, durante as fortes chuvas que atingiram a cidade na tarde de quarta-feira (27).",
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
 1, 1),
-("São Paulo", "São Paulo Futebol Clube é um clube poliesportivo brasileiro da cidade de São Paulo, capital do estado homônimo. Foi fundado em 25 de janeiro de 1930, tendo interrompido suas atividades em maio de 1935, e as retomado em dezembro do mesmo ano.",
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
 2, 2),
-("Transformers", "Uma nova ameaça capaz de destruir todo o planeta surge fazendo com que Optimus Prime e os Autobots se unam a uma poderosa facção de Transformers conhecida como Maximals para salvar a Terra.",
-3, 3);
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+3, 3),
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+4, 4),
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+5, 5),
+("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+6, 6);
