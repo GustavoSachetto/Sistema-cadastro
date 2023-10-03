eccce68b796cbf87b4552b5c15f99fdff954a79d
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-03 20:27:05 -0300

    Update cadastrarRecebe.php

--- controller/cadastrarRecebe.php
+++ controller/cadastrarRecebe.php
@@ -1,12 +1,15 @@
 <?php 
-    require_once('../model/conexao.php');
+    require_once('model/conexao.php');
 
-    $config = parse_ini_file('../model/config.ini');
+    $config = parse_ini_file('model/config.ini');
     $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
 
-    $cadCategoria = addslashes($_POST['txtCategoria']);
-    $cadTitulo = addslashes($_POST['txtTitulo']);
-    $cadConteudo = addslashes($_POST['txtConteudo']);
-
-    $conexao -> insereNoticia ($cadCategoria, $cadTitulo, $cadConteudo);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+        $cadCategoria = addslashes($_POST['txtCategoria']);
+        $cadTitulo = addslashes($_POST['txtTitulo']);
+        $cadConteudo = addslashes($_POST['txtConteudo']);
+    
+        $conexao -> insereNoticia ($cadCategoria, $cadTitulo, $cadConteudo);
+    }
 ?>
