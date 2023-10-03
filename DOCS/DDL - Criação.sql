6260dcb22b6fdbef9c2dd4098fa2ae0ea4ee2275
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-03 20:26:13 -0300

    Update DDL - Criação.sql

--- DOCS/DDL - Criação.sql
+++ DOCS/DDL - Criação.sql
@@ -3,15 +3,20 @@ create database projetonoticia;
 use projetonoticia;
 
 create table categoria (
-	tipo char(20) not null unique,
-    id int auto_increment not null primary key
+    id int auto_increment not null primary key,
+	tipo char(20) not null unique
 );
 
 create table noticia (
+    codNoticia int auto_increment not null primary key,
 	titulo varchar(30) not null,
     conteudo varchar(250) not null,
-    codNoticia int auto_increment not null primary key,
+    
+    constraint uqNoticia
+	unique (titulo, conteudo, idCategoria),
     
     idCategoria int not null,
 	constraint fkidCategoria foreign key (idCategoria) references categoria (id)
 );
+
+-- drop database projetonoticia;
