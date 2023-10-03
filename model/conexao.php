b62e159fd512a5f3ba93279ac3050b1d590ac87a
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-03 20:24:54 -0300

    Update conexao.php

--- model/conexao.php
+++ model/conexao.php
@@ -1,10 +1,11 @@
 <?php 
     class conexao {
         private $pdo;
+
         public function __construct($dbname, $host, $user, $password) {
             try {
                 $this -> pdo = new PDO ("mysql:dbname=" . $dbname . "; host=" . $host, $user, $password);
-                echo "Conexão do PDO";
+                echo "";
             } catch (PDOException $erro) {
                 echo "Erro de conexão no PDO: " . $erro -> getMessage();
                 exit();
@@ -13,13 +14,42 @@
                 exit();
             }
         }
+        
+        public function consultaBanco($consulta) {
+            $consultaBanco = $this -> pdo -> query($consulta);
+            $consultaBanco -> execute();
 
+            while ($resultado = $consultaBanco -> fetchAll(PDO::FETCH_ASSOC)) {
+                return $resultado;
+            }
+        }
+
+        public function consultaCategoria($categoria) {
+            $consulta = "SELECT * FROM categoria WHERE tipo = '$categoria'";
+            $resultado = $this -> consultaBanco($consulta);
+            
+            if (boolval($resultado) === false) {
+                $this -> insereCategoria($categoria);
+                $resultado = $this -> consultaBanco($consulta);
+            } 
+            
+            return $resultado[0]['id'];
+        }
+        
+        public function insereCategoria($categoria) {
+            $insereCategoria = $this -> pdo -> prepare("insert into categoria(tipo) value (:tipo)");
+            $insereCategoria -> bindValue(':tipo', $categoria);
+            $insereCategoria -> execute();
+        }
+        
         public function insereNoticia ($cadCategoria, $cadTitulo, $cadConteudo) {
-            $insereNoticia = $this -> pdo -> prepare("insert into categoria(tipo) value (:categoria); insert into noticia(titulo, conteudo) values (:titulo, :conteudo)");
+            $insereNoticia = $this -> pdo -> prepare("insert into noticia(titulo, conteudo, idCategoria) values (:titulo, :conteudo, :idCategoria)");
 
-            $insereNoticia -> bindValue(':categoria', $cadCategoria);
+            $idCategoria = $this -> consultaCategoria($cadCategoria);
+            
             $insereNoticia -> bindValue(':titulo', $cadTitulo);
             $insereNoticia -> bindValue(':conteudo', $cadConteudo);
+            $insereNoticia -> bindValue(':idCategoria', $idCategoria);
             $insereNoticia -> execute();
         }
     }
