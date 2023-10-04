fe3c0dd6b2f7baa4dc458ab17df541d5933ab004
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-04 20:54:38 -0300

    Update DML - Consulta.sql

--- DOCS/DML - Consulta.sql
+++ DOCS/DML - Consulta.sql
@@ -1,7 +1,9 @@
+use projetonoticia;
+
 select * from categoria;
 
 select * from noticia;
 
 select count(*) from noticia;
 
-select titulo, conteudo , idCategoria from noticia where codNoticia = 1;
+select titulo, conteudo , tipoCategoria from noticia where codNoticia = 1;
