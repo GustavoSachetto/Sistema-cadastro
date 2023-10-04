9815302177a3d0001386678281190bf8ae186918
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-04 20:54:59 -0300

    Update noticias.php

--- controller/noticias.php
+++ controller/noticias.php
@@ -5,38 +5,45 @@
     $conexao = new conexao ($config['dbname'], $config['host'], $config['user'], $config['password']);
 
     const CATEGORIA = "SELECT * FROM categoria ORDER BY id ASC";
-    const NOTICIA = "SELECT * FROM noticia ORDER BY codNoticia ASC";
+    const NOTICIA = "SELECT * FROM noticia";
 
     $categorias = $conexao -> consultaBanco(CATEGORIA);
 
     if (boolval($categorias) === true) {
 
-        $noticias = $conexao -> consultaBanco(NOTICIA);
+        $noticias = $conexao -> consultaBanco(NOTICIA . $busca);
         
-        function verificaTipo($categorias, $idCategoria) {
-            foreach ($categorias as $categoria) {
-                if ($idCategoria === $categoria['id']) {
-                    return $categoria['tipo'];
+        if (empty($noticias) == true) {
+            echo "<h2>Não foi possivel localizar nenhuma notícia que contenha: <a href='cadastrarNoticias.php'>$termoBusca</a></h2>";
+        } else {
+            function verificaTipo($categorias, $tipoCategoria) {
+                foreach ($categorias as $categoria) {
+                    if ($tipoCategoria === $categoria['tipo']) {
+                        return $categoria['tipo'];
+                    }
                 }
             }
+    
+            echo "<main><article>";
+            foreach ($noticias as $noticia) {
+                $titulo = $noticia['titulo'];
+                $conteudo = $noticia['conteudo'];
+                $tipo = verificaTipo($categorias, $noticia['tipoCategoria']);
+    
+                echo "
+                    <section>
+                        <div>
+                            <h2>$titulo</h2>
+                            <h3>$tipo</h3>
+                            <p>$conteudo</p>
+                        </div>
+                        <a href='' class='btn-padrao'>Acessar</a>
+                    </section>
+                ";
+            }
+            echo "</article></main>";
         }
 
-        foreach ($noticias as $noticia) {
-            $titulo = $noticia['titulo'];
-            $conteudo = $noticia['conteudo'];
-            $tipo = verificaTipo($categorias, $noticia['idCategoria']);
-
-            echo "
-                <section>
-                    <div>
-                        <h2>$titulo</h2>
-                        <h3>$tipo</h3>
-                        <p>$conteudo</p>
-                    </div>
-                    <a href='' class='btn-padrao'>Acessar</a>
-                </section>
-            ";
-        }
     } else {
         echo "<h2>Não há notícias cadastradas! <a href='cadastrarNoticias.php'>Cadastrar Notícias.</a></h2>";
     }
