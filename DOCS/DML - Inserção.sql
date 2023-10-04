859626cc0242de50dcababca3f98f3764b8a1662
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-04 20:54:28 -0300

    Update DML - Inserção.sql

--- DOCS/DML - Inserção.sql
+++ DOCS/DML - Inserção.sql
@@ -1,22 +1,26 @@
+use projetonoticia;
+
 insert into categoria (tipo, id) values
 ("Esporte", 1),
-("Ação", 2),
+("Urgente", 2),
 ("Filmes e séries", 3),
-("Aventura", 4),
+("Política", 4),
 ("Drama", 5),
 ("Comédia", 6),
-("Terror", 7);
+("Jogos e Desenhos", 7);
 
-insert into noticia (titulo, conteudo, codNoticia, idCategoria) values
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-1, 1),
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-2, 2),
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-3, 3),
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-4, 4),
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-5, 5),
-("Titulo da noticia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
-6, 6);
+insert into noticia (titulo, conteudo, codNoticia, tipoCategoria) values
+("Neymar na Arabia", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+1, "Esporte"),
+("Greve dos metroviários", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+2, "Urgente"),
+("Novo filme da Barbie", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+3, "Filmes e séries"),
+("Prefeito de Alagoas", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+4, "Política"),
+("A morte do Pelé", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+5, "Drama"),
+("Sogra de Eddie Murphy", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+6, "Comédia"),
+("Crise no Free Fire", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime in obcaecati molestiae ullam facere architecto saepe animi facilis! Dolores quidem porro quae cupiditate maxime minima repudiandae temporibus deleniti exercitationem voluptatum!",
+7, "Jogos e Desenhos");
