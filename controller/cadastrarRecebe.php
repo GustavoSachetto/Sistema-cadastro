e61cdf385abd6c221e8366e0be4d071d7ebaaa34
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-06 20:26:57 -0300

    Update

--- controller/cadastrarRecebe.php
+++ controller/cadastrarRecebe.php
@@ -1,16 +1,3 @@
-<main>
-    <article>
-        <form action="" method="post">
-            <fieldset>
-                <h2>Cadastrar notícia</h2>
-                <input type="text" name="txtTitulo" id="txtTitulo" minlength="4" maxlength="30" placeholder="Titulo da notícia" required>
-                <input type="text" name="txtCategoria" id="txtCategoria" minlength="2" maxlength="20" placeholder="Categoria da notícia" required>
-                <textarea name="txtConteudo" id="txtConteudo" minlength="30" maxlength="250" placeholder="Conteúdo da notícia" cols="30" rows="10" required></textarea>
-            </fieldset>
-            <button type="submit" class="btn-padrao">Cadastrar</button>
-        </form>
-    </article>
-</main>
 <?php 
     require_once('model/conexao.php');
 
