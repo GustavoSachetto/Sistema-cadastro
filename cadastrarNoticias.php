034ae924336227a489c4b3b0d0172d7cf5ab2952
GustavoSachetto <g.sachettocruz@gmail.com>
2023-10-10 15:13:58 -0300

    add

--- cadastrarNoticias.php
+++ cadastrarNoticias.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sistema-cadastro - Cadastrar</title>
 
+        <link rel="shortcut icon" href="img/lupa.png" type="image/x-icon">
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/form.css">
     </head>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Sistema-cadastro - Início</title>
 
+        <link rel="shortcut icon" href="img/lupa.png" type="image/x-icon">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
