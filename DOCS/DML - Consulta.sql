a6c0c2a330fa6f103be35000ae038d3bfdd6d380
Gustavo Sachetto <136517074+GustavoSachetto@users.noreply.github.com>
2023-11-12 18:12:44 -0300

    Update DML - Consulta.sql

--- DOCS/DML - Consulta.sql
+++ DOCS/DML - Consulta.sql
@@ -7,3 +7,6 @@ select * from noticia;
 select count(*) from noticia;
 
 select titulo, conteudo , idCategoria from noticia where codNoticia = 1;
+
+SELECT noticia.titulo, noticia.conteudo, categoria.tipo FROM noticia
+INNER JOIN categoria ON noticia.idCategoria = categoria.id;
